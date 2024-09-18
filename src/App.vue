9b69ae84fb7cb25407e2000a263ca99b3a50cf01
6988116@qq.com <6988116@qq.com>
2024-09-18 14:14:08 +0800

    搭建项目脚手架

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
-    <h1>vue-element-plus-admin-manage</h1>
+	<h1>vue-element-plus-admin-manage</h1>
 </template>
-  
-<script setup lang='ts'>
-  
-</script>
-  
-<style>
-  
-</style>
+
+<script setup lang="ts"></script>
+
+<style></style>
